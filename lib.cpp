6ebb355a09866a1bf8ce88f7a091a48a92c078fb
nicosicca <100024661+nicosicca@users.noreply.github.com>
2022-05-02 18:42:32 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+#include "lib.h"
 
+bool conversione(char &a){
+    if(((a>=65)&&(a<=90))||((a>=97)&&(a<=122)))
+    {
+        if((a>=65)&&(a<=90))
+            a=a+32;
+        else
+            a=a-32;
+
+        return true;
+    }
+    else
+        return false;
+}
