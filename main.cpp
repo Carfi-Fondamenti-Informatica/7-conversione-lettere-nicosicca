5997695221ade0eb46ffe210e5431d3f0a3d9bf2
nicosicca <100024661+nicosicca@users.noreply.github.com>
2022-05-02 18:41:26 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+
+int main() {
+
+    char a;
+    cin>>a;
+
+    if(conversione(a))
+        cout<<a;
+    else
+        cout<<"errore"<<endl;
+
+    return 0;
 }
