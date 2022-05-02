f53a6f5ee6a57882990d8d35e5d368d01fb043cd
nicosicca <100024661+nicosicca@users.noreply.github.com>
2022-05-02 19:13:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
-#include "lib.h"
 using namespace std;
-
 
 
 int main() {
 
     char a;
     cin>>a;
-    
+
     if(conversione(a))
-        cout<<a;
+    {
+        if((a>=65)&&(a<=90))
+            a=a+32;
+        else
+            a=a-32;
+        
+        cout<<(char)a;
+    }
+        
     else
         cout<<"errore"<<endl;
 
     return 0;
 }
+
